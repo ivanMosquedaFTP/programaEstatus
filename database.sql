0d0d937988b4f9bd575163290015b3de797d3bfe
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-17 19:49:00 -0600

    added database script

--- database.sql
+++ database.sql
@@ -9,3 +9,15 @@ create table egresado(no_control int auto_increment primary key, nombre_completo
     status tinyint,
     fecha_examen date
 );
+
+create table sinodal(id_sinodal int auto_increment primary key, nombre_completo varchar(255));
+
+create table asesor(id_asesor int auto_increment primary key, nombre_completo varchar(255));
+
+create table proyecto(id_proyecto int auto_increment primary key, nombre_proyecto varchar(255));
+
+alter table egresado add column asesor integer references asesor(id_asesor);
+alter table egresado add column nombre_proyecto integer references proyecto(id_proyecto);
+alter table egresado add column sinodal_1 integer references sinodal(id_sinodal);
+alter table egresado add column sinodal_2 integer references sinodal(id_sinodal);
+alter table egresado add column sinodal_3 integer references sinodal(id_sinodal);
--- db.sql
+++
@@ -1,1 +0,0 @@
-/home/toreve/DataGripProjects/proyectoIngSoftware/db.sql
