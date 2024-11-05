961ad25a61d05d13f5824693a69b542a306aa1df
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-04 18:33:49 -0600

    added db configuration

--- config.class.php
+++ config.class.php
@@ -1,8 +1,8 @@
 <?php
     define("SGBD", 'mysql');
     define("DBHOST",'localhost');
-    define("DBNAME",'crops');
-    define("DBUSER",'crops');
+    define("DBNAME",'sistema_titulaciones');
+    define("DBUSER",'sistema_titulaciones');
     define("DBPASS",'1234');
     define("DBPORT",'3306');
 ?>
