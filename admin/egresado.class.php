089b7e5131958f76abbff356cad226ea99421869
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-04 20:48:55 -0600

    refactored egresado.class.php and egresado.php renamed

--- admin/egresado.class.php
+++ admin/egresado.class.php
@@ -27,14 +27,20 @@
     function update ($id, $data){
         $this->conexion();
         $result = [];
-        $sql = 'update egresado set no_control=:no_control, nombre_completo=:nombre_completo, especialidad=:especialidad, nombre_proyecto=:nombre_proyecto, opc_titulacion=:opc_titulacion, status=:status, fecha_examen=:fecha_examen, asesor=:asesor, sinodal1=:sinodal1, sinodal2=:sinodal2, sinodal3=:sinodal3 where no_control=:no_control;';
+        $sql = 'update egresado set no_control=:no_control, nombre_completo=:nombre_completo, especialidad=:especialidad, nombre_proyecto=:nombre_proyecto, opc_titulacion=:opc_titulacion, status=:status, fecha_examen=:fecha_examen, asesor=:asesor, sinodal1=:sinodal1, sinodal2=:sinodal2, sinodal3=:sinodal3 where id_solicitud=:id_solicitud;';
         $modificar=$this->con->prepare($sql);
-        $modificar->bindParam(':id_especialidad',$id, PDO::PARAM_INT);
-        $modificar->bindParam(':especialidad',$data['especialidad'], PDO::PARAM_STR);
-        $modificar->bindParam(':opc_titulacion',$data['opc_titulacion'], PDO::PARAM_STR);
-        $modificar->bindParam(':nombre_proyecto',$data['nombre_proyecto'], PDO::PARAM_STR);
-        $modificar->bindParam(':no_control',$data['no_control'], PDO::PARAM_INT);
+        $modificar->bindParam(':no_control',$data['nombre_completo'], PDO::PARAM_STR);
         $modificar->bindParam(':nombre_completo',$data['nombre_completo'], PDO::PARAM_STR);
+        $modificar->bindParam(':especialidad',$data['especialidad'], PDO::PARAM_STR);
+        $modificar->bindParam(':nombre_proyecto',$data['nombre_proyecto'], PDO::PARAM_STR);
+        $modificar->bindParam(':opc_titulacion',$data['opc_titulacion'], PDO::PARAM_STR);
+        $modificar->bindParam(':status',$data['status'], PDO::PARAM_INT);
+        $modificar->bindParam(':fecha_examen',$data['fecha_examen'], PDO::PARAM_STR);
+        $modificar->bindParam(':asesor',$data['asesor'], PDO::PARAM_STR);
+        $modificar->bindParam(':sinodal1',$data['sinodal1'], PDO::PARAM_STR);
+        $modificar->bindParam(':sinodal2',$data['sinodal2'], PDO::PARAM_STR);
+        $modificar->bindParam(':sinodal3',$data['sinodal3'], PDO::PARAM_STR);
+        $modificar->bindParam(':id_solicitud',$id, PDO::PARAM_INT);
         $modificar->execute();
         $result= $modificar->rowCount();
         return $result;
@@ -43,9 +49,9 @@
     function delete ($id){
         $this -> conexion();
         $result = [];
-        $sql = "delete from especialidad where id_especialidad=:id_especialidad;";
+        $sql = "delete from especialidad where id_solicitud=:id_solicitud;";
         $eliminar = $this->con->prepare($sql);
-        $eliminar -> bindParam(':id_especialidad', $id, PDO::PARAM_INT);
+        $eliminar -> bindParam(':id_solicitud', $id, PDO::PARAM_INT);
         $eliminar -> execute();
         $result = $eliminar -> rowCount();
         return $result;
@@ -54,7 +60,7 @@
     function readOne ($id){
         $this->conexion();
         $result = [];
-        $consulta = 'SELECT * FROM especialidad where id_especialidad=:id_especialidad;';
+        $consulta = 'SELECT * FROM egresado where id_especialidad=:id_especialidad;';
         $sql = $this->con->prepare($consulta);
         $sql->bindParam(":id_especialidad",$id,PDO::PARAM_INT);
         $sql -> execute();
@@ -66,7 +72,7 @@
     function readAll (){
         $this -> conexion();
         $result = [];
-        $consulta ='select * from especialidad';
+        $consulta ='select * from egresado';
         $sql = $this->con->prepare ($consulta); 
         $sql -> execute();
         $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
