1f487b0580d556b13c9396e3414968af6edc0a8c
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-04 18:36:25 -0600

    deleted files from previous filesystem

--- admin/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('../sistema.class.php');
-    $app = new sistema;
-    $roles = $app->getRole('luislao@itcelaya.edu.mx');
-    print_r($roles);
-    $privilegio=$app->getPrivilegios('l21031178@celaya.tecnm.mx');
-    print_r($privilegio);
-?>
