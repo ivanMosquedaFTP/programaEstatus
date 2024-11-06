c8a0ae240ba875f41339893d33f872268bb717fb
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-05 20:56:16 -0600

    fixed home reference at footer

--- admin/views/footer_home.php
+++ admin/views/footer_home.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <a class="btn btn-danger w-100" href="index.php">Volver al inicio</a>
+            <a class="btn btn-danger w-100" href="egresado.php">Volver al inicio</a>
         </div>
         <div class="col-md-4"></div>
     </div>
