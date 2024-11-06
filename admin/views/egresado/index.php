228f9683e9fc0f2287105cabbec12ec54c696002
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-05 18:28:32 -0600

    added modular header and footer

--- admin/views/egresado/index.php
+++ admin/views/egresado/index.php
@@ -1,28 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Titulacion</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../css/main.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
-</head>
-
-<body>
-  <header>
-    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-      <div class="container-fluid py-5 jumbotron">
-        <h1 class="display-5 fw-bold">Bienvenido al sistema de consulta de titulaciones</h1>
-        <p class="col-md-8 fs-4">Este sistema permite la gestión de egresados aspirantes a titulación</p>
-      </div>
-    </div>
-  </header>
-
+  <?php require('views/header.php');?>
   <main>
     <section>
       <div class="row">
@@ -42,6 +18,4 @@
       </div>
     </section>
   </main>
-</body>
-
-</html>
+<?php require('views/footer.php');?>
--- admin/views/header.php
+++ admin/views/header.php
@@ -1,9 +1,24 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-    <title></title>
-  </head>
-  <body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Titulacion</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../css/main.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <header>
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div class="container-fluid py-5 jumbotron">
+        <h1 class="display-5 fw-bold">Bienvenido al sistema de control de titulaciones</h1>
+        <p class="col-md-8 fs-4">Este sistema permite la gestión de egresados aspirantes a titulación</p>
+      </div>
+    </div>
+  </header>
