5b4702fc73bc02934746190b238bd7a7faa17d80
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-15 22:18:05 -0600

    added en construccion page

--- admin/views/egresado/index.php
+++ admin/views/egresado/index.php
@@ -8,11 +8,11 @@
           <br> <br>
           <a class="btn btn-primary btn-lg w-100" href="egresado.php?accion=premodificacion">Modificar información de un egresado.</a>
           <br> <br>
-          <a class="btn btn-primary btn-lg w-100" href="consultarEstatus.php">Consulta por estatus.</a>
+          <a class="btn btn-primary btn-lg w-100" href="views/egresado/construccion.php">Consulta por estatus.</a>
           <br> <br>
-          <a class="btn btn-primary btn-lg w-100" href="consultarOpc.php">Consulta por opción de titulación.</a>
+          <a class="btn btn-primary btn-lg w-100" href="views/egresado/construccion.php">Consulta por opción de titulación.</a>
           <br> <br>
-          <a class="btn btn-primary btn-lg w-100" href="consultarPorNoControl.php">Consulta por número de control.</a>
+          <a class="btn btn-primary btn-lg w-100" href="views/egresado/construccion.php">Consulta por número de control.</a>
         </div>
         <div class="col-md-4"></div>
       </div>
