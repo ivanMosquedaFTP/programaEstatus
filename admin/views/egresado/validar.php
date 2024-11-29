8d1a22f86def64af17bc40497528a7445fa753c5
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-29 14:20:12 -0600

    added input character limit at validar.php

--- admin/views/egresado/validar.php
+++ admin/views/egresado/validar.php
@@ -1,30 +1,40 @@
 <?php require('views/header.php'); ?>
 <center>
-    <h1>Primero introduzca el numero de control del egresado aspirante a titulacion que desea modificar</h1>
+  <h1>Primero introduzca el numero de control del egresado aspirante a titulacion que desea modificar</h1>
 </center>
 
 <br>
 <main>
-    <br>
-    <section class="formulario">
-        <form action="egresado.php?accion=validar" method="POST">
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                    <input type="number" name="data[no_control]" required="true" class="form-control" id="noControl" aria-describedby="noControlHelp">
-                </div>
-                <div class="col-md-4"></div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                    <input type="submit" name="data[enviar]" required="true" class="btn btn-primary w-100" value="Verificar">
-                </div>
-                <div class="col-md-4"></div>
-            </div>
-        </form>
-    </section>
+  <br>
+  <section class="formulario">
+    <form action="egresado.php?accion=validar" method="POST">
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <input type="text" name="data[no_control]" required class="form-control" id="noControl" maxlength="8" oninput="validarNoControl(this);">
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <input type="submit" name="data[enviar]" required="true" class="btn btn-primary w-100" value="Verificar">
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+    </form>
+  </section>
 </main>
+
+<script>
+  function validarNoControl(input) {
+    input.value = input.value.replace(/\D/g, '');
+
+    if (input.value.length > 8) {
+      input.value = input.value.slice(0, 8);
+    }
+  }
+</script>
 
 <?php require('views/footer_home.php'); ?>
