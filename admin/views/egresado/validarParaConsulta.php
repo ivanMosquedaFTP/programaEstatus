53b54493c197989c04322b95590b7b8c76b6144b
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-29 13:53:36 -0600

    ci

--- admin/views/egresado/validarParaConsulta.php
+++ admin/views/egresado/validarParaConsulta.php
@@ -1,37 +1,37 @@
 <?php require('views/header.php'); ?>
 <center>
-    <h1>Primero introduzca el estatus que desea consultar.</h1>
+  <h1>Primero introduzca el estatus que desea consultar.</h1>
 </center>
 
 <br>
 <main>
-    <br>
-    <section class="formulario">
-        <form action="egresado.php?accion=consultaPorStatus" method="POST">
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                    <select name="data[status]" required="true" class="form-control" id="status">
-                        <?php
-                            echo(<option></>);
-                        for ($i = 0; $i <= 6; $i++) {
-                            echo "<option value=\"$i\">$i.- echo($data[status]);</option>";
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="col-md-4"></div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                    <input type="submit" name="data[enviar]" required="true" class="btn btn-primary w-100" value="Consultar">
-                </div>
-                <div class="col-md-4"></div>
-            </div>
-        </form>
-    </section>
+  <br>
+  <section class="formulario">
+    <form action="egresado.php?accion=consultaPorStatus" method="POST">
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <select name="data[status]" required="true" class="form-control" id="status">
+            <?php
+            /*echo ("<option value=$i>$i</option>");*/
+            for ($i = 0; $i <= 6; $i++) {
+              echo "<option value=\"$i\">$i.- echo($data[status]);</option>";
+            }
+            ?>
+          </select>
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <input type="submit" name="data[enviar]" required="true" class="btn btn-primary w-100" value="Consultar">
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+    </form>
+  </section>
 </main>
 
 <?php require('views/footer_home.php'); ?>
--- util/dml.sql
+++ util/dml.sql
@@ -1,1 +1,2 @@
 select * from egresado;
+desc egresado;
