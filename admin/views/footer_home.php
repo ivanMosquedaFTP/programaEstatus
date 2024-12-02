0f1ab61540a10ebe27c62abc371de1ec306b017a
CaracheoAdan <21031400@itcelaya.edu.mx>
2024-12-01 22:47:38 -0600

    Confirmaciones al btn de volver al inicio

--- admin/views/footer_home.php
+++ admin/views/footer_home.php
@@ -1,12 +1,12 @@
     <br> <br>
 
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <a class="btn btn-danger w-100" href="egresado.php">Volver al inicio</a>
-        </div>
-        <div class="col-md-4"></div>
+    <div class="row"> 
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <a class="btn btn-danger w-100" href="egresado.php" onclick="return confirm('¿Estás seguro de que deseas volver al inicio?');">Volver al inicio</a>
     </div>
+    <div class="col-md-4"></div>
+</div>
 
     <br>
 </body>
